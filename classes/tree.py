304e363dae9e61a5f20938edf5a0be99bb97c3a3
mkirc <m.p.kirchner@gmx.de>
2020-03-09 23:54:48 +0100

    1800x speedup due to max() outside loop in split(), and primer towards formal validation

--- classes/tree.py
+++ classes/tree.py
@@ -1,6 +1,8 @@
 import numpy
 import math
 
+
+
 class kdTree():
 
 	def __init__(self, depth, divCrit, startAxis):
@@ -38,6 +40,10 @@ class kdTree():
 		for l in self.root.getLeaves():
 			self.leaves.append(l)
 
+	def postOrderWalk(self):
+
+		self.root.postOrderWalk()
+
 
 		
 
@@ -45,22 +51,26 @@ class kdTree():
 
 class Node():
 
+	NODE_ID = 1
+	NODE_INFO = []
+
 	def __init__(self, parent):
 
+		self.id = Node.NODE_ID
 		self.depth = 0
 		self.isLeaf = True
 		self.leftChild = None
 		self.rightChild = None
 		self.parent = parent
 		self.points = []
-		# self.leaves = []
+
+		Node.NODE_ID += 1
 
 
 	def getMax(self, axis):
 
-		cur_max = 0
-		for p in self.points:
-			cur_max = max(cur_max, p.dim[axis])
+		cur_max = max([p.dim[axis] for p in self.points])
+
 		return cur_max
 
 
@@ -74,6 +84,14 @@ class Node():
 			yield from self.rightChild.getLeaves()
 			yield from self.leftChild.getLeaves()	
 
+	def postOrderWalk(self):
+
+		if self.leftChild:
+			self.leftChild.postOrderWalk()
+		if self.rightChild:
+			self.rightChild.postOrderWalk()
+
+		Node.NODE_INFO.append((self.id, len(self.points)))
 
 	def split(self, depth, axis, divCrit):
 
@@ -82,16 +100,25 @@ class Node():
 				self.leftChild = Node(self)
 				self.rightChild = Node(self)
 				self.isLeaf = False
-				for point in self.points:
+				try:
 					divisor = divCrit * self.getMax(axis)
-					if point.dim[axis] <= divisor:
-						self.leftChild.points.append(point)
-					else:
-						self.rightChild.points.append(point)
-				depth = depth - 1
-				axis = (axis + 1) % 3
-				self.leftChild.split((depth), axis, divCrit)
-				self.rightChild.split((depth), axis, divCrit)
+				except ValueError:
+					# enter smart error handling here
+					# probably this only happens when the 
+					# divisor gets too small
+					# print(depth)
+					pass
+				finally:
+					for point in self.points:
+						if point.dim[axis] <= divisor:
+							self.leftChild.points.append(point)
+						else:
+							self.rightChild.points.append(point)
+					
+					depth = depth - 1
+					axis = (axis + 1) % 3
+					self.leftChild.split((depth), axis, divCrit)
+					self.rightChild.split((depth), axis, divCrit)
 		else:
 			depth = depth - 1
 			axis = (axis + 1) % 3
--- run.py
+++ run.py
@@ -30,20 +30,43 @@ class TreeController():
 		print('initial total Volume:		%.4e' % self.initialTotalVolume)
 		print('initial total DeadVolume:	%.4e' % self.initialTotalDeadVolume)
 		print('Number of Leaves:			%s' % len(self.tree.leaves))
+		self.sortNodeInfo()
+		y = 0
+		for n in Node.NODE_INFO:
+			x = int(math.log2(n[0]))
+			if x > y:
+				print('')
+				print(n[0], n[1], " ", end = '')
+				y = x
+			else:
+				print(n[0] , n[1] , " ", end = '')
+		print('')
+			
+
+	def sortNodeInfo(self):
+
+		Node.NODE_INFO.sort(key=lambda tup: tup[0])
+
+		
 
 def run():
 
-	depth = 3
-	divCrit = 0.6
+	depth = 4
+	divCrit = 0.1
 	startAxis = 0
+	numPoints = 47000
 
 	t = TreeController('assets/raw_data_01.csv')
 	t.getInitialValues()
 	t.initializeTree(depth, divCrit, startAxis)
 	p = [i[0] for i in t.itemBoxes]
 	# print([i.dim[1] for i in p[0:10]])
-	t.tree.insert(p[0:10])
+	t.tree.insert(p[0:numPoints])
+
+
 	t.tree.grow()
+	t.tree.postOrderWalk()
+
 	t.printInfo()
 
 
