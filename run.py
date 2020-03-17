adds volume comparison, writing to outfile, corrects volume calculation, needs cleanup

run.py
@@ -16,6 +16,10 @@ class TreeController():
 		self.itemBoxes = self.pf.getItemBoxes()
 		self.tree = None
 		self.bestNodes = []
+		self.newItemBoxes = None
+		self.newTotalDeadVolume = 0
+		self.newTotalVolume = 0
+		self.gain = 0
 
 	def initializeTree(self, d, c, s):
 
@@ -23,9 +27,9 @@ class TreeController():
 
 	def getInitialValues(self):
 
-		self.initialTotalVolume = np.sum(self.itemBoxes[:][0]).vol
-		self.initialTotalDeadVolume =  (self.initialTotalVolume
-										- np.sum(self.itemBoxes[:][1]).vol)
+		self.initialTotalVolume = np.sum([b[0].vol for b in self.itemBoxes])
+		self.initialTotalDeadVolume =  (np.sum([b[1].vol for b in self.itemBoxes])
+										- self.initialTotalVolume)
 
 	def getDeltaVs(self, bestN=None):
 
@@ -44,7 +48,7 @@ class TreeController():
 		
 		for mvp in self.getDeltaVs():
 			dV, n = mvp
-			self.bestNodes.append((n, dV))
+			self.bestNodes.append((n.id, n, dV))
 			if dV == 0:
 				break
 		return
@@ -63,13 +67,79 @@ class TreeController():
 		print('')
 		return
 
+	def writeOutNewItemBoxes(self, path):
+
+		print('start writing...')
+		self.tree.leaves.sort(key=lambda node:node.id)
+		bestNodesCopy = [i for i in self.bestNodes]
+
+		with open(path, 'w+') as openFile:
+
+			
+			bestNodesCopy.sort(key=lambda tup:tup[0])
+
+			for l in self.tree.leaves:
+
+				if l.id < bestNodesCopy[0][0]:
+					
+					kNom = 'KARTON' + ' ' + str(self.bestNodes[0][0])
+					kDimX = bestNodesCopy[0][1].dim[0]
+					kDimY = bestNodesCopy[0][1].dim[1]
+					kDimZ = bestNodesCopy[0][1].dim[2]
+
+					for point in l.points:
+
+						pDimX = point.dim[0]
+						pDimY = point.dim[1]
+						pDimZ = point.dim[2]
+
+						line = '%s,%s,%s,%s,%s,%s,%s,\n' % (kNom,kDimX,kDimY,kDimZ,pDimX,pDimY,pDimZ)
+
+						openFile.write(line)
+
+				elif l.id == bestNodesCopy[0][0]:
+
+					kNom = 'KARTON' + ' ' + str(self.bestNodes[0][0])
+					kDimX = bestNodesCopy[0][1].dim[0]
+					kDimY = bestNodesCopy[0][1].dim[1]
+					kDimZ = bestNodesCopy[0][1].dim[2]
+
+					for point in l.points:
+						
+						pDimX = point.dim[0]
+						pDimY = point.dim[1]
+						pDimZ = point.dim[2]
+
+						line = '%s,%s,%s,%s,%s,%s,%s,\n' % (kNom,kDimX,kDimY,kDimZ,pDimX,pDimY,pDimZ)
+
+						openFile.write(line)
+
+					lastNode = bestNodesCopy.pop(0)
+					# print('last Node: %s. Only %s to go!' % (lastNode[0], len(self.bestNodes)))
+			else:
+
+				print('✔ finished writing %s' % (path))
+				print('')
+				return
+
+	def getNewValues(self, path):
+
+		self.pf.loadPoints(path, new=True)
+		self.newItemBoxes = self.pf.getNewItemBoxes()
+		self.newTotalVolume = np.sum([b[0].vol for b in self.newItemBoxes])
+		self.newTotalDeadVolume =  (np.sum([b[1].vol for b in self.newItemBoxes])
+										- self.newTotalVolume)
+		self.gain = self.newTotalDeadVolume / self.initialTotalDeadVolume
+
+
 
 	def printInfo(self, numPoints, extended=False, bestN=False):
 
-		print('Number of Points:\t\t\t%.2e' % numPoints)
+		print('Number of Points:\t\t\t%i' % numPoints)
 		print('initial total Volume:\t\t%.4e' % self.initialTotalVolume)
 		print('initial total DeadVolume:\t%.4e' % self.initialTotalDeadVolume)
 		print('Number of Leaves:\t\t\t%s' % len(self.tree.leaves))
+
 		
 		if extended:
 
@@ -90,6 +160,10 @@ class TreeController():
 
 			print(' Leaves with deltaV gain:	%i' % (len(self.bestNodes)))
 
+		print('')
+		print('new total Volume:\t\t\t%.4e' % self.newTotalVolume)
+		print('new total DeadVolume:\t\t%.4e' % self.newTotalDeadVolume)
+		print('Thats like...%.3f of the initial!' % self.gain)
 
 
 
@@ -102,9 +176,12 @@ def run():
 	depth = 13
 	divCrit = 0.5
 	startAxis = 0
-	numPoints = 47000
+	numPoints = 47287
+
+	outPath = 'assets/new_boxes_86.csv'
 
 	t = TreeController('assets/raw_data_01.csv')
+
 	t.getInitialValues()
 	t.initializeTree(depth, divCrit, startAxis)
 	p = [i[0] for i in t.itemBoxes]
@@ -117,6 +194,9 @@ def run():
 	t.isNumPointsConst()
 
 	t.getBestNodes()
+
+	t.writeOutNewItemBoxes(outPath)
+	t.getNewValues(outPath)
 
 	t.printInfo(numPoints, bestN=True)
 